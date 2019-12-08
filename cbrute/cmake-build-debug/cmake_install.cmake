563172c51cffdcaa1cf231fd4e721bc622ed79c5
SchnJulian <julian-schnabel@posteo.de>
2019-12-08 12:41:08 +0100

    init clion

--- cbrute/cmake-build-debug/cmake_install.cmake
+++ cbrute/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/Julian/Documents/Workspace/CLion Workspace/cbrute
+# Install script for directory: /Users/Julian/Documents/GitHub/cbrute/cbrute
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/Julian/Documents/Workspace/CLion Workspace/cbrute/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/Julian/Documents/GitHub/cbrute/cbrute/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
